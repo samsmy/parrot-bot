85eb716fa27c5ea0c268cf167878fb5f36f27d0d
Samuel Smy <samuel.smy@bglgroup.co.uk>
2019-01-16 12:13:08 +0000

    Next commit

---
+++ index.js
@@ -0,0 +1,66 @@
+var SlackBot = require('slackbots');
+var request = require("request");
+
+
+
+
+// create a bot
+const envKey = process.env.JOKES_BOT_TOKEN //"xoxb-524985330466-525189685397-UWwRkrjOWAw85BVMNJFGkKBG"//process.env.JOKES_BOT_TOKEN
+var bot = new SlackBot({
+  token: envKey,
+  name: 'parrot Bot'
+})
+
+/* bot.on('start', function() 
+{
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    var params = {
+        icon_emoji: ':cat:'
+    };
+    
+    // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
+    bot.postMessageToChannel('general', 'meow!', params);
+    
+    // define existing username instead of 'user_name'
+    //bot.postMessageToUser('user_name', 'meow!', params); 
+    
+    // If you add a 'slackbot' property, 
+    // you will post to another user's slackbot channel instead of a direct message
+   // bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
+    
+    // define private group instead of 'private_group', where bot exist
+    //bot.postMessageToGroup('private_group', 'meow!', params); 
+}
+
+); */
+
+bot.on("message", msg => {
+    switch (msg.type) {
+    case "message":
+      if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+        getRandomJoke(postMessage, msg.user)
+      }
+
+      
+      break
+    }
+  })
+
+
+  const postMessage = (message, user) => {
+    bot.postMessage(user, message, { as_user: true })
+  }
+
+  const getRandomJoke = (callback, user) => {
+    return request("https://icanhazdadjoke.com/slack", (error, response) => {
+      if (error) {
+        console.log("Error: ", error)
+      } else {
+        let jokeJSON = JSON.parse(response.body)
+        let joke = jokeJSON.attachments[0].text
+        console.log(joke);
+        return callback(joke, user)
+      }
+    })
+  }
+  
